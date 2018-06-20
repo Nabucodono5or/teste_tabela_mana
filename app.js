728066d1b199cbb81f989a6006073cc5c3cae1ab
Jefferson Abreu <pereirajefferson06@gmail.com>
2018-06-19 23:26:20 -0300

    criado base do teste

---
+++ app.js
@@ -0,0 +1,15 @@
+(function () {
+  
+  angular.module('myApp', []);
+
+})();
+
+(function () {
+
+  function myAppController() {
+    
+  }
+
+  myAppController.$inject = [];
+  angular.module('myApp').controller('myAppController', myAppController);
+})();
